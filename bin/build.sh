bd92e65abc878a18a6bad1fc034a6e97a3ed3fd7
Larpoux <larpoux@gmail.com>
2022-04-20 09:49:50 +0200

    TAU : Version 9.2.9

--- bin/build.sh
+++ bin/build.sh
@@ -7,3 +7,8 @@ cd rust
 cbindgen ./src/lib.rs -c cbindgen.toml | grep -v \#include | uniq > target/bindings.h
 cd ..
 
+
+cd ios
+xcodebuild -scheme taudio-waa_native clean
+xcodebuild -scheme taudio-waa_native build
+cd ..
