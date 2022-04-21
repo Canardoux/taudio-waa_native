347db3b05a83595c045f306db62dfa9a5d758c6f
Larpoux <larpoux@gmail.com>
2022-04-21 23:47:46 +0200

    Taudio-aaa_native : Version 0.0.30

--- ios/taudio-waa_native/bindings.cpp
+++
@@ -1,7 +0,0 @@
-
-//#include  "bindings.h"
-// This will never be executed
-//extern "C" void dummyMethodToEnforceBundling ()
-//{
-//        rust_greeting("toto");
-//}
--- ios/taudio-waa_native/bindings.h
+++
@@ -1,6 +0,0 @@
-
-//extern "C" {
-
-//char *rust_greeting(const char *to);
-
-//} // extern "C"
